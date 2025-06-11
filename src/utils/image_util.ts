11ccbb7cae714ffef855ea1ab8658b522874d0d7
aabbPhD <aabb22phd@mail.ru>
2025-06-11 15:34:52 +0300

    .

--- src/utils/image_util.ts
+++ src/utils/image_util.ts
@@ -1,3 +1,9 @@
 export default function getImageURL(name: string) {
+    //для github-pages
+    if (import.meta.env.PROD) {
+        const basePath = import.meta.env.BASE_URL;
+        return `${basePath}img/products/${name}`
+    }
+    //для localhost
     return new URL('../assets/img/products/' + name, import.meta.url).href;
 }
